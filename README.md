e91b51861a36938f73661894f0eb2e9e936ef4a9
JackChen <jackchen1113@ntu.edu.tw>
2025-10-09 15:26:39 +0800

    完整V1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ntu_calendar
 台大行事曆試作_eva01
+
--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('AlpineData', () =>
     ({
+        weekdayNames: ['日', '一', '二', '三', '四', '五', '六'],
+        Events: [],
+        viewDate: dayjs(),
+        days: [],
+        Start() {
+            this.Events = [
+                { id: 1, date: '2023-10-01', title: '活動一' },
+                { id: 2, date: '2023-10-05', title: '活動二' },
+                { id: 3, date: '2023-10-10', title: '活動三' }
+            ];
+            this.renderDays();
+        },
+        //
+        renderDays() {
+            console.log(this.viewDate);
+            const startOfMonth = this.viewDate.startOf('month');
+            const startCalendar = startOfMonth.startOf('week');
+            this.days = Array.from({ length: 42 }, (_, i) => startCalendar.add(i, 'day'));
 
+            //console.log(this.days);
+        },
+
+
+        prevMonth() {
+            this.viewDate = this.viewDate.subtract(1, 'month');
+            this.renderDays();
+        },
+        nextMonth() {
+            this.viewDate = this.viewDate.add(1, 'month');
+            this.renderDays();
+        },
+        today() {
+            this.viewDate = dayjs();
+            this.renderDays();
+        },
 
     }))
 })
