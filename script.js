f30d34d63495fa104dc98f24557c2cca06574458
JackChen <jackchen1113@ntu.edu.tw>
2025-10-10 15:54:36 +0800

    小補丁

--- script.js
+++ script.js
@@ -41,6 +41,16 @@ document.addEventListener('alpine:init', () => {
         },
         //創造月曆陣列
         renderDays() {
+            //一週的第一天是禮拜天(切換成台灣)
+            dayjs.locale('zh-tw', {
+                weekStart: 0
+            });
+            //如果要切換成禮拜一開始，改成1
+            //dayjs.locale('zh-tw', {
+            //    weekStart: 1
+            //});
+            this.getMonthActivity();
+            //取得本月第一天
             const startOfMonth = this.viewDate.startOf('month');//取得本月第一天
             const startCalendar = startOfMonth.startOf('week');//取得日曆的開始日期(本月第一天的禮拜天)
             this.days = Array.from({ length: 42 }, (_, i) => startCalendar.add(i, 'day'));//建立一個包含42天的陣列(6週)
